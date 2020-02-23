f989e428f5123ce4a312b87bb09d4a35cafcb359
Javier Bull <javierbullreyes@gmail.com>
2020-02-23 17:49:35 -0500

    evento load

---
+++ js/load.js
@@ -0,0 +1,24 @@
+// alert("Hola");
+
+window.addEventListener('load', function() {
+  function elegirColor(){
+
+    var estadoActivador = document.getElementById('activador').checked;
+
+    if (estadoActivador == true) {
+      document.getElementById('verde').disabled=false;
+      document.getElementById('amarillo').disabled=false;
+      document.getElementById('azul').disabled=false;
+    } else {
+      document.getElementById('verde').disabled=true;
+      document.getElementById('amarillo').disabled=true;
+      document.getElementById('azul').disabled=true;
+    }
+  };
+
+  // Ejecutando la función
+
+  var activador = document.getElementById('activador');
+
+  activador.addEventListener('change', elegirColor);
+});
