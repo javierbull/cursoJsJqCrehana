21a1c65ad896002a7aa91717bb2b4d5cd9e348cb
Javier Bull <javierbullreyes@gmail.com>
2020-06-13 12:26:41 -0500

    css con Jquery

---
+++ jquery/cssjquery.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  // alert('Hola');
+
+  // AÑADIR CLASE A UN ELEMENTO
+  // $('#segundo').addClass('fondo-rojo');
+
+  // ELIMINAR UNA CLASE A UN ELEMENTO
+  // $('#segundo').removeClass('texto');
+
+  // ALTERNAR ENTRE ELIMINAR O AGREGAR UNA CLASE
+  // function cambiarFondo(){
+  //   $('#segundo').toggleClass('fondo-rojo');
+  // }
+  //
+  // var btn = $('#boton');
+  //
+  // btn.click(cambiarFondo);
+
+  // ====================================
+
+  // CAMBIANDO UNA SOLA PROPIEDAD
+  // $('#segundo').css('background', 'blue');
+
+  // CAMBIANDO VARIAS PROPIEDADES
+  $('#segundo').css({
+    background: 'gold',
+    color: 'purple'
+  })
+
+
+});
