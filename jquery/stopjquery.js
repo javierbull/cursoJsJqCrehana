5614f78562683dbd09c2fadfc8aafb89fe0be930
Javier Bull <javierbullreyes@gmail.com>
2020-06-13 20:58:13 -0500

    funcion stop con jquery

--- jquery/stopjquery.js
+++ jquery/stopjquery.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
   // alert('Holas');
 
-  
+  var btn = $('#boton');
+  var stop = $('#detener');
+
+  btn.on('click', function(){
+    $('#segundo').animate({
+      width:'200%'
+    },5000);
+
+    $('#segundo').animate({
+      width:'300%'
+    },5000);
+
+    $('#segundo').animate({
+      width:'100%'
+    },5000);
+
+  });
+
+  stop.on('click', function(){
+
+    // .stop(limpiar cola, saltar al final);
+
+    $('#segundo').stop(true, false);
+  });
+
 
 });
